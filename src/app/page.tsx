39a1a5f51d1ef299ad455bdc34f69c00217bcf1b
Jamoladdin <jamoladdinisnatdinov2@gmail.com>
2024-09-01 22:48:57 +0500

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,68 @@
 'use client'
 
-export default function AdminLoginForm() {
+import $api from '@/api/axios'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import Admin from './admin'
+import AdminLoginForm from './login'
+
+export default function AdminPage() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [loading, setLoading] = useState(true)
+
+	const refreshAccessToken = async () => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			setIsLoggedIn(false)
+			setLoading(false)
+			return
+		}
+
+		try {
+			const response = await $api.get('/auth/refresh', {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			const { id, username, fullName, image } = response.data
+			if (id && username && fullName && image) {
+				setIsLoggedIn(true)
+			} else {
+				setIsLoggedIn(false)
+			}
+		} catch (error) {
+			setIsLoggedIn(false)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		refreshAccessToken()
+	}, [])
+
+	if (loading) {
+		return <div>Loading...</div>
+	}
+
 	return (
-		<h1 className='text-6xl flex justify-center items-center h-screen'>
-			No content
-		</h1>
+		<div className='overflow-x-hidden'>
+			<Head>
+				<title>{isLoggedIn ? 'Admin Dashboard' : 'Admin Login'}</title>
+				<meta
+					name='description'
+					content={
+						isLoggedIn
+							? 'qabulga yozilish, hokim qabulga yozilish, qabulga yozilish.uz'
+							: 'qabulga yozilish, hokim qabulga yozilish, qabulga yozilish.uz'
+					}
+				/>
+			</Head>
+			{isLoggedIn ? (
+				<Admin setIsLoggedIn={setIsLoggedIn} />
+			) : (
+				<AdminLoginForm setIsLoggedIn={setIsLoggedIn} />
+			)}
+		</div>
 	)
 }
